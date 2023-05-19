13eaf0df34fc170fc9ed89c1ae6ae386cbbccb40
Mladen <jerbdroid@gmail.com>
2023-05-18 23:01:09 -0400

    Added premake script

---
+++ premake5.lua
@@ -0,0 +1,40 @@
+project "GLFW"
+	kind "StaticLib"
+	language "C"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"include/GLFW/*.h", 
+		"src/*.h",
+		"src/*.c"
+	}
+	filter "system:linux"
+		pic "On"
+
+		systemversion "latest"
+		staticruntime "On"
+
+		defines
+		{
+			"_GLFW_X11"
+		}
+
+	filter "system:windows"
+		systemversion "latest"
+		staticruntime "On"
+
+		defines 
+		{ 
+			"_GLFW_WIN32"
+		}
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
